06fadc19b6ea237514d67eafd945fe40420bb383
fhgaha <antsy1992@gmail.com>
2022-08-06 14:45:43 +0300

    tg2 remove p1res p2res

--- csharp/Tennis/TennisGame2/TennisGame2.cs
+++ csharp/Tennis/TennisGame2/TennisGame2.cs
@@ -7,9 +7,6 @@ namespace Tennis
         private TennisGame2Folder.Player player1;
         private TennisGame2Folder.Player player2;
 
-        private string p1res = "";
-        private string p2res = "";
-
         Dictionary<int, string> scoreTerms;
 
         public TennisGame2(string player1Name, string player2Name)
@@ -45,53 +42,16 @@ namespace Tennis
 
             if (player1.Points > 0 && player1.Points < 4 && player2.Points == 0)
             {
-                if (player1.Points == 1)
-                    p1res = "Fifteen";
-                if (player1.Points == 2)
-                    p1res = "Thirty";
-                if (player1.Points == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
                 score = scoreTerms[player1.Points] + "-Love";
             }
             if (player2.Points > 0 && player2.Points < 4 && player1.Points == 0)
             {
-                if (player2.Points == 1)
-                    p2res = "Fifteen";
-                if (player2.Points == 2)
-                    p2res = "Thirty";
-                if (player2.Points == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
                 score = "Love-" + scoreTerms[player2.Points];
             }
 
-            if (player1.Points > player2.Points && player1.Points < 4)
+            if (player1.Points > player2.Points && player1.Points < 4
+                || player2.Points > player1.Points && player2.Points < 4)
             {
-                if (player1.Points == 2)
-                    p1res = "Thirty";
-                if (player1.Points == 3)
-                    p1res = "Forty";
-                if (player2.Points == 1)
-                    p2res = "Fifteen";
-                if (player2.Points == 2)
-                    p2res = "Thirty";
-
-                score = scoreTerms[player1.Points] + "-" + scoreTerms[player2.Points];
-            }
-            if (player2.Points > player1.Points && player2.Points < 4)
-            {
-                if (player2.Points == 2)
-                    p2res = "Thirty";
-                if (player2.Points == 3)
-                    p2res = "Forty";
-                if (player1.Points == 1)
-                    p1res = "Fifteen";
-                if (player1.Points == 2)
-                    p1res = "Thirty";
-
                 score = scoreTerms[player1.Points] + "-" + scoreTerms[player2.Points];
             }
 
